662f742568311954b74b54fa2a6e87a9c903585d
Shyam-Chen <shyamchen1994@gmail.com>
2024-05-05 19:46:11 +0800

    6th Commit

--- examples/basic/src/routes/+page.svelte
+++ examples/basic/src/routes/+page.svelte
@@ -10,14 +10,12 @@
   let form = $state<Form>({});
   let valdn = $state<Partial<Record<keyof Form, string>>>({});
 
-  const msgs = { required: 'This is a required field' };
+  let msgs = $state({ required: 'This is a required field' });
 
-  let struct = $derived.by(() =>
-    v.object({
-      name: v.nullish(v.string([v.minLength(1, msgs.required)]), ''),
-      email: v.nullish(v.string([v.minLength(1, msgs.required)]), ''),
-    }),
-  );
+  let struct = v.object({
+    name: v.nullish(v.string([v.minLength(1, msgs.required)]), ''),
+    email: v.nullish(v.string([v.minLength(1, msgs.required)]), ''),
+  });
 
   const schema = useSchema(struct, form, valdn);
 
@@ -25,6 +23,10 @@
     if (schema.validate()) {
       console.log('passed =', form);
     }
+  };
+
+  const i18n = () => {
+    msgs.required = '這是必填欄位';
   };
 </script>
 
@@ -39,3 +41,6 @@
 
 <pre>{JSON.stringify(form, null, 2)}</pre>
 <pre>{JSON.stringify(valdn, null, 2)}</pre>
+<pre>{JSON.stringify(msgs, null, 2)}</pre>
+
+<button type="button" class="mt-6" onclick={i18n}>i18n</button>
